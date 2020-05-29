0fb835d7f5b6e7c0b6d856861f03b088d40166d8
davebaol <davix7@yahoo.it>
2020-05-29 15:24:23 +0200

    Enable import of json modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import form from './initial-form.json';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-formio-editor-demo';
+  form: any;
+
+  constructor() {
+    this.form = form;
+  }
+
 }
